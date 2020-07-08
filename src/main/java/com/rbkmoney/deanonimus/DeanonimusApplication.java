21799f4f58f3df9fea970fd0cbb4b53954609766
NPospolita <nikita7asics@gmail.com>
2020-07-08 12:45:33 +0300

    init

---
+++ src/main/java/com/rbkmoney/deanonimus/DeanonimusApplication.java
@@ -0,0 +1,15 @@
+package com.rbkmoney.deanonimus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan
+@SpringBootApplication
+public class DeanonimusApplication extends SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeanonimusApplication.class, args);
+    }
+
+}
