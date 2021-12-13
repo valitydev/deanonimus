9d68e432efda580bba9d209d3202c26f444f5a2f
ggmaleva <griwania@gmail.com>
2021-12-13 15:33:13 +0300

    remove redundant

--- src/test/java/com/rbkmoney/deanonimus/AbstractIntegrationTest.java
+++ src/test/java/com/rbkmoney/deanonimus/AbstractIntegrationTest.java
@@ -25,40 +25,15 @@ import java.util.Map;
 @ExtendWith({KafkaContainerExtension.class, ElasticsearchContainerExtension.class})
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @SpringBootTest
-//@ContextConfiguration(initializers = AbstractIntegrationTest.Initializer.class)
 public abstract class AbstractIntegrationTest {
 
     private static final String TOPIC_NAME = "mg-events-party";
-
-//    private static final String CONFLUENT_PLATFORM_VERSION = "5.0.1";
-//
-//    @ClassRule
-//    public static ElasticsearchContainer elastic =
-//            new ElasticsearchContainer("docker.elastic.co/elasticsearch/elasticsearch:7.8.0");
-//
-//    @ClassRule
-//    public static KafkaContainer kafka = new KafkaContainer(CONFLUENT_PLATFORM_VERSION).withEmbeddedZookeeper();
 
     @DynamicPropertySource
     static void containersProps(DynamicPropertyRegistry registry) {
         registry.add("kafka.bootstrap-servers", KafkaContainerExtension.KAFKA::getBootstrapServers);
         registry.add("spring.elasticsearch.rest.uris",
                 ElasticsearchContainerExtension.ELASTIC_SEARCH::getHttpHostAddress);
-    }
-//
-//    public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-//        @SneakyThrows
-//        @Override
-//        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
-//            TestPropertyValues
-//                    .of("kafka.bootstrap-servers=" + kafka.getBootstrapServers(),
-//                            "spring.elasticsearch.rest.uris=" + elastic.getHttpHostAddress())
-//                    .applyTo(configurableApplicationContext.getEnvironment());
-//        }
-//    }
-
-    public static void sendMessage(SinkEvent sinkEvent) {
-        createProducer().send(new ProducerRecord<>(TOPIC_NAME, sinkEvent.getEvent().getSourceId(), sinkEvent));
     }
 
     public static void sendMessages(List<SinkEvent> sinkEvents) {
