964a80f7f309f4ee13246e3eeba73241ed815a08
malkoas <alien.life.form.1986@gmail.com>
2023-11-27 14:13:34 +0300

    Add JsonIgnoreProperties

--- src/main/java/dev/vality/deanonimus/domain/Contract.java
+++ src/main/java/dev/vality/deanonimus/domain/Contract.java
@@ -1,5 +1,6 @@
 package dev.vality.deanonimus.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Contract {
     private String id;
     private String contractorId;
--- src/main/java/dev/vality/deanonimus/domain/Wallet.java
+++ src/main/java/dev/vality/deanonimus/domain/Wallet.java
@@ -1,5 +1,6 @@
 package dev.vality.deanonimus.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Wallet {
     private String id;
     private String name;
