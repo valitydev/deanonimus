4d171b2f1f6feee934975bc9288f3e18738ea731
malkoas <alien.life.form.1986@gmail.com>
2023-11-28 10:52:34 +0300

    Del nested query

--- src/main/java/dev/vality/deanonimus/db/SearchDaoImpl.java
+++ src/main/java/dev/vality/deanonimus/db/SearchDaoImpl.java
@@ -5,12 +5,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.opensearch.client.opensearch.OpenSearchClient;
-import org.opensearch.client.opensearch._types.query_dsl.*;
+import org.opensearch.client.opensearch._types.query_dsl.BoolQuery;
+import org.opensearch.client.opensearch._types.query_dsl.MultiMatchQuery;
+import org.opensearch.client.opensearch._types.query_dsl.Query;
+import org.opensearch.client.opensearch._types.query_dsl.TextQueryType;
 import org.opensearch.client.opensearch.core.SearchResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import static dev.vality.deanonimus.constant.OpenSearchConstants.*;
 
 @Slf4j
 @Component
@@ -44,33 +45,27 @@ public class SearchDaoImpl implements SearchDao {
     }
 
     private Query searchContractorFields(String text) {
-        return new NestedQuery.Builder()
-                .path(CONTRACTOR_INDEX)
-                .query(new Query(new MultiMatchQuery.Builder()
-                        .fields("contractors.id",
-                                "contractors.registeredUserEmail",
-                                "contractors.russianLegalEntityRegisteredName",
-                                "contractors.russianLegalEntityInn",
-                                "contractors.russianLegalEntityRussianBankAccount",
-                                "contractors.internationalLegalEntityLegalName",
-                                "contractors.internationalLegalEntityTradingName")
-                        .query(text)
-                        .type(TextQueryType.Phrase)
-                        .build()))
-                .build().query();
+        return new Query(new MultiMatchQuery.Builder()
+                .fields("contractors.id",
+                        "contractors.registeredUserEmail",
+                        "contractors.russianLegalEntityRegisteredName",
+                        "contractors.russianLegalEntityInn",
+                        "contractors.russianLegalEntityRussianBankAccount",
+                        "contractors.internationalLegalEntityLegalName",
+                        "contractors.internationalLegalEntityTradingName")
+                .query(text)
+                .type(TextQueryType.Phrase)
+                .build());
     }
 
     private Query searchContractFields(String text) {
-        return new NestedQuery.Builder()
-                .path(CONTRACT_INDEX)
-                .query(new Query(new MultiMatchQuery.Builder()
-                        .fields("contracts.id",
-                                "contracts.legalAgreementId",
-                                "contracts.reportActSignerFullName")
-                        .query(text)
-                        .type(TextQueryType.Phrase)
-                        .build()))
-                .build().query();
+        return new Query(new MultiMatchQuery.Builder()
+                .fields("contracts.id",
+                        "contracts.legalAgreementId",
+                        "contracts.reportActSignerFullName")
+                .query(text)
+                .type(TextQueryType.Phrase)
+                .build());
     }
 
     private Query searchPartyFields(String text) {
@@ -83,28 +78,22 @@ public class SearchDaoImpl implements SearchDao {
     }
 
     private Query searchShopFields(String text) {
-        return new NestedQuery.Builder()
-                .path(SHOP_INDEX)
-                .query(new Query(new MultiMatchQuery.Builder()
-                        .fields("shops.id",
-                                "shops.locationUrl",
-                                "shops.detailsName")
-                        .query(text)
-                        .type(TextQueryType.Phrase)
-                        .build()))
-                .build().query();
+        return new Query(new MultiMatchQuery.Builder()
+                .fields("shops.id",
+                        "shops.locationUrl",
+                        "shops.detailsName")
+                .query(text)
+                .type(TextQueryType.Phrase)
+                .build());
     }
 
     private Query searchWalletFields(String text) {
-        return new NestedQuery.Builder()
-                .path(WALLET_INDEX)
-                .query(new Query(new MultiMatchQuery.Builder()
-                        .fields("wallets.id",
-                                "wallets.name")
-                        .query(text)
-                        .type(TextQueryType.Phrase)
-                        .build()))
-                .build().query();
+        return new Query(new MultiMatchQuery.Builder()
+                .fields("wallets.id",
+                        "wallets.name")
+                .query(text)
+                .type(TextQueryType.Phrase)
+                .build());
     }
 
 }
