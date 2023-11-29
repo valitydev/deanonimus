391b1992c13f6b0ffe8402ce7948e29445704742
malkoas <41993717+malkoas@users.noreply.github.com>
2023-11-29 11:58:00 +0300

    Update SearchDaoImpl.java

--- src/main/java/dev/vality/deanonimus/db/SearchDaoImpl.java
+++ src/main/java/dev/vality/deanonimus/db/SearchDaoImpl.java
@@ -1,7 +1,6 @@
 package dev.vality.deanonimus.db;
 
 import dev.vality.deanonimus.domain.Party;
-import dev.vality.deanonimus.domain.Shop;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
